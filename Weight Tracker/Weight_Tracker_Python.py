92f28286d3f6720b33b60987ce56d26476a88a16
Simon <152021717+SimonR-prog@users.noreply.github.com>
2024-01-23 22:21:38 +0100

    Start
    Starting the weight tracker. Defining what is needed.

--- Weight Tracker/Weight_Tracker_Python.py
+++ Weight Tracker/Weight_Tracker_Python.py
@@ -1,30 +1,3 @@
-"""
-Weight tracker.
-
-Utilize json to save the date, weight whether or not I went on a walk/exercised.
-
-Normal date format.
-Weight will be tracked with integer.
-Can maybe use walk and train as bools.
-
-Functions needed;
-- An exporter that creates and write to json.file.
-- Importer to bring in the information to print it and to utilize it.
-- UI;
-- Calculations to calc the weight week by week. And how much progress in total.
-
-
-While loop;
-- Clear the console.
-- Show the progress.
-
-"""
-
-
-
-
-
-
 
 
 
