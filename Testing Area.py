c79b241e511c6a791b9312fd61cccbecb5c44260
Simon <152021717+SimonR-prog@users.noreply.github.com>
2024-01-27 23:28:00 +0100

    Update 27-jan
    Lost some progress from yesterday because of windows update. Added two functions to get current day in textform, such as "monday", and one to get the current week.

--- Testing Area.py
+++ Testing Area.py
@@ -1,11 +1,22 @@
-import datetime
+def current_week():
+    from datetime import date
+    week = date.today().isocalendar()[1]
+    return week
 
 
-from datetime import date
-date = date.today()
-print(date.weekday())
 
 
-week = date.today().isocalendar()[1]
+print(current_week())
 
-print(week)
+
+
+
+def current_day():
+    from datetime import date
+    date = date.today()
+    weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    day = weekdays[(date.weekday())]
+    return day
+
+
+print(current_day())
--- Weight Tracker/Weight_Tracker_Python.py
+++ Weight Tracker/Weight_Tracker_Python.py
@@ -1,6 +1,6 @@
 import json
-import datetime
 import os
+
 def clear_console(): # Function to clear the console.
     if os.name == 'nt':
         os.system('cls')
@@ -26,7 +26,7 @@ def writer(today_dict):
 
 def summary():
     # Function to use on sundays to sum up the previous weeks data and put into json.file.
-
+    new_data = ""
     with open('weight_summary.json', 'w') as f:
         f.write(json.dumps(new_data))
 
@@ -40,29 +40,40 @@ def ui():
     data_dict = importer()
     print("")
 
-def dict_creator(date, weight, walk, gym):
-    # Function to make todays dict with data and send to write function.
-    # Find out which day it is and make that the dictionaries name
+def current_week():
+    # Function to return current week.
+    from datetime import date
+    week = date.today().isocalendar()[1]
+    return week
+
+def current_day():
+    # Function to return current day.
     from datetime import date
     date = date.today()
-    day = date.weekday()
-     = {}
+    weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    day = weekdays[(date.weekday())]
+    return day
 
-    today_dict = {}
-    today_dict["date"] = f"{date}"
-    today_dict["weight"] = weight
-    today_dict["walk"] = walk
-    today_dict["gym"] = gym
+def dict_creator(weight, walk, gym):
+    # Function to add today's data to the variables and add them to the dictionary.
+    # Need to have one dict for each week as well inside another dict that sums up the data for that week.
 
-    writer(today_dict)
+
+    day = current_day()
+    dict_todays_data = {}
+    dict_todays_data[day] = {}
+    # Inserting data as values to keys;
+    dict_todays_data[day]["weight"] = weight
+    dict_todays_data[day]["walk"] = walk
+    dict_todays_data[day]["gym"] = gym
+    # Sending todays updated data to the next function.
+    writer(dict_todays_data)
 
 def data_input():
-    from datetime import date
-    date = date.today()
     weight = int(input("How much do you weigh? > "))
     walk = float(input("How far did you walk? > "))
     gym = int(input("Did you go to the gym? (1 for yes, 0 for no.) > "))
-    dict_creator(date, weight, walk, gym)
+    dict_creator(weight, walk, gym)
 
 
 data_input()
